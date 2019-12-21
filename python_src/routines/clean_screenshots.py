4b45df5a5db4c4fd22c16960b7b545c547224429
David Scroggins <david.j.scroggins@gmail.com>
2019-12-20 19:18:09 -0600

    Custom logging kwargs for clean_screenshots

--- python_src/routines/clean_screenshots.py
+++ python_src/routines/clean_screenshots.py
@@ -5,6 +5,11 @@ from modules.directory_cleaner import DirectoryCleaner
 
 BASE_DIR = Path('/Users/davidscroggins/Desktop/screenshots')
 
+LOG_CONFIG = {
+    'logs_subdir': 'screenshots-cleaning-logs',
+    'logs_file_name': 'screenshots-cleaning'
+}
+
 if __name__ == "__main__":
-    dc = DirectoryCleaner(base_dir=BASE_DIR, max_age=180, preview=True)
+    dc = DirectoryCleaner(base_dir=BASE_DIR, max_age=180, preview=True, **LOG_CONFIG)
     dc.remove_old_content()
