ce6a6b419fb199bbc2608b69a12f6439fdff346a
David Scroggins <david.j.scroggins@gmail.com>
2019-12-20 19:16:30 -0600

    Custom logging kwargs for clean_downloads

--- python_src/routines/clean_downloads.py
+++ python_src/routines/clean_downloads.py
@@ -5,6 +5,11 @@ from modules.directory_cleaner import DirectoryCleaner
 
 BASE_DIR = Path('/Users/davidscroggins/Downloads')
 
+LOG_CONFIG = {
+    'logs_subdir': 'downloads-cleaning-logs',
+    'logs_file_name': 'downloads-cleaning'
+}
+
 if __name__ == "__main__":
-    dc = DirectoryCleaner(base_dir=BASE_DIR, max_age=180, preview=True)
+    dc = DirectoryCleaner(base_dir=BASE_DIR, max_age=180, preview=True, **LOG_CONFIG)
     dc.remove_old_content()
