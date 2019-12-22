02cbedc6c51d30da1a55a03b9c34b3f669c8ffe8
David Scroggins <david.j.scroggins@gmail.com>
2019-12-22 15:19:36 -0600

    Removed deactive from setup script

--- python_src/py-env-setup.sh
+++ python_src/py-env-setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-deactivate && python -m venv .env && source $_/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
+python -m venv .env && source $_/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
