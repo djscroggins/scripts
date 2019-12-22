65cf4cbdc24b4aa97b743ce33ae90f4182a1986c
David Scroggins <david.j.scroggins@gmail.com>
2019-12-22 13:22:42 -0600

    Log time and conditional success message in bash scripts

--- clean-downloads.sh
+++ clean-downloads.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 source scripts.env
 cd python_src
-$(pwd)/.env/bin/python setup_logs.py && $(pwd)/.env/bin/python routines/clean_downloads.py
+date
+if $(pwd)/.env/bin/python setup_logs.py && $(pwd)/.env/bin/python routines/clean_downloads.py ; then
+    echo "SUCCESS"
+fi
--- clean-screenshots.sh
+++ clean-screenshots.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 source scripts.env
 cd python_src
-$(pwd)/.env/bin/python setup_logs.py && $(pwd)/.env/bin/python routines/clean_screenshots.py
+date
+if $(pwd)/.env/bin/python setup_logs.py && $(pwd)/.env/bin/python routines/clean_screenshots.py ; then
+    echo "SUCCESS"
+fi
