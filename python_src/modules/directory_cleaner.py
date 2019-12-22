94e65bd8601b1f02aff3c0be8fe761cb4d47eb0b
David Scroggins <david.j.scroggins@gmail.com>
2019-12-22 15:10:50 -0600

    Refactored isntance variables to indicate private, added some missing type annotations

--- python_src/modules/directory_cleaner.py
+++ python_src/modules/directory_cleaner.py
@@ -26,16 +26,16 @@ class DirectoryCleaner:
             preview (bool): when set to True, will only generate a report of operation to be performed
             debug (bool): when set to True, logs will be streamed to console rather than written to file
         """
-        self.total_contents = 0
-        self.total_old_contents = 0
-        self.files_deleted = []
-        self.dirs_deleted = []
-        self.bad_metadata = []
-        self.osx_system_content = {'.DS_Store', '.localized', '__MACOSX'}
-        self.base_dir = base_dir if isinstance(base_dir, Path) else Path(base_dir).resolve()
-        self.max_age = dt.timedelta(days=max_age).total_seconds()
-        self.now = time.time()
-        self.preview = preview
+        self._total_contents = 0
+        self._total_old_contents = 0
+        self._files_deleted = []
+        self._dirs_deleted = []
+        self._bad_or_missing_metadata = []
+        self._osx_system_content = {'.DS_Store', '.localized', '__MACOSX'}
+        self._target_dir = base_dir if isinstance(base_dir, Path) else Path(base_dir).resolve()
+        self._max_age = dt.timedelta(days=max_age).total_seconds()
+        self._now = time.time()
+        self._preview = preview
 
         logger_kwargs = dict(
             logger_name=kwargs.get('logger_name', type(self).__name__),
@@ -43,17 +43,17 @@ class DirectoryCleaner:
             logs_subdir_name=kwargs.get('logs_subdir', 'directory-cleaning-logs'),
             log_file_name=kwargs.get('logs_file_name', 'directory-cleaning')
         )
-        self.logger = self._get_logger(logging.DEBUG if debug else logging.INFO, **logger_kwargs)
+        self._logger = self._get_logger(logging.DEBUG if debug else logging.INFO, **logger_kwargs)
 
     @staticmethod
     def _get_logger(log_level: int, **kwargs) -> logging.Logger:
         """
-        Returns configured logger. Streams to stdout if set to DEBUG, else writes to log file.
+        Returns configured _logger. Streams to stdout if set to DEBUG, else writes to log file.
         Args:
             log_level (int): desired log level
 
         Returns:
-            Configured logger
+            Configured _logger
 
         """
         logger_name = kwargs.get('logger_name')
@@ -82,31 +82,31 @@ class DirectoryCleaner:
         try:
             os.remove(file_path)
         except Exception as e:
-            self.logger.error(str(e))
-            self.logger.error(tb.format_exc())
+            self._logger.error(str(e))
+            self._logger.error(tb.format_exc())
             sys.exit('Error executing clean up')
 
     def _remove_directory(self, dir_path: Union[Path, os.PathLike]) -> None:
         try:
             shutil.rmtree(dir_path)
         except Exception as e:
-            self.logger.error(str(e))
-            self.logger.error(tb.format_exc())
+            self._logger.error(str(e))
+            self._logger.error(tb.format_exc())
             sys.exit('Error executing clean up')
 
-    def _handle_old_content(self, content_path: Path, timestamp) -> None:
-        content_age = self.now - timestamp
-        if content_age > self.max_age:
-            self.total_old_contents += 1
-            self.logger.debug(f'{content_path}: {content_age}')
+    def _handle_old_content(self, content_path: Path, timestamp: float) -> None:
+        content_age = self._now - timestamp
+        if content_age > self._max_age:
+            self._total_old_contents += 1
+            self._logger.debug(f'{content_path}: {content_age}')
             if Path(content_path).is_file():
-                if not self.preview:
+                if not self._preview:
                     self._remove_file(content_path)
-                self.files_deleted.append(str(content_path))
+                self._files_deleted.append(str(content_path))
             elif Path(content_path).is_dir():
-                if not self.preview:
+                if not self._preview:
                     self._remove_directory(content_path)
-                self.dirs_deleted.append(str(content_path))
+                self._dirs_deleted.append(str(content_path))
 
     @staticmethod
     def _get_osx_date_added(content_path: Path) -> str:
@@ -131,32 +131,32 @@ class DirectoryCleaner:
         return str(cp.stdout.decode('utf-8'))
 
     def log_results(self) -> None:
-        self.logger.info(f'\n**** SUMMARY ****'
-                         f'\nTotal Contents: {self.total_contents}\n'
-                         f'Total Contents to be Deleted: {self.total_old_contents}\n'
-                         f'Files Deleted: {self.files_deleted}\n'
-                         f'Directories (and contents) Deleted: {self.dirs_deleted}\n'
-                         f'Bad/Missing Metadata: {self.bad_metadata}\n')
+        self._logger.info(f'**** SUMMARY ****\n'
+                          f'Total Contents: {self._total_contents}\n'
+                          f'Total Contents to be Deleted: {self._total_old_contents}\n'
+                          f'Files Deleted: {self._files_deleted}\n'
+                          f'Directories (and contents) Deleted: {self._dirs_deleted}\n'
+                          f'Bad/Missing Metadata: {self._bad_or_missing_metadata}\n')
 
     def remove_old_content(self) -> None:
-        self.logger.info(f'Cleaning {self.base_dir}')
-        directory_contents = os.listdir(self.base_dir)
+        self._logger.info(f'Cleaning {self._target_dir}')
+        target_objects: list = os.listdir(self._target_dir)
 
-        for content in directory_contents:
-            self.total_contents += 1
+        for target_object in target_objects:
+            self._total_contents += 1
 
-            content_path = self.base_dir.joinpath(content)
-            date_added = self._get_osx_date_added(content_path)
+            target_path = self._target_dir.joinpath(target_object)
+            date_added = self._get_osx_date_added(target_path)
 
-            if content not in self.osx_system_content:
+            if target_object not in self._osx_system_content:
                 if 'null' in date_added:
-                    self.bad_metadata.append(str(content_path))
-                    last_modified_timestamp = os.stat(content_path).st_mtime
-                    self._handle_old_content(content_path, last_modified_timestamp)
+                    self._bad_or_missing_metadata.append(str(target_path))
+                    last_modified_timestamp: float = os.stat(target_path).st_mtime
+                    self._handle_old_content(target_path, last_modified_timestamp)
 
                 else:
-                    date_added_timestamp = dt.datetime.strptime(
+                    date_added_timestamp: float = dt.datetime.strptime(
                         date_added, '%Y-%m-%d %H:%M:%S %z').timestamp()
-                    self._handle_old_content(content_path, date_added_timestamp)
+                    self._handle_old_content(target_path, date_added_timestamp)
 
         self.log_results()
