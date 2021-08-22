88ed44aeb054f6e21f3c579b7e8d391b5ef541b1
David Scroggins <david.j.scroggins@gmail.com>
2021-08-22 09:08:44 -0500

    Update modules to black formatting

--- python_src/setup_logs.py
+++ python_src/setup_logs.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from os import mkdir
 
-log_dir = Path(__file__).resolve().parent.joinpath('logs')
+log_dir = Path(__file__).resolve().parent.joinpath("logs")
 if not log_dir.exists():
     mkdir(log_dir)
